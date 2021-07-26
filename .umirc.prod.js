82d11b299f5fd4548010409278225b79cc8e9a93
yunpei.wang@miaocode.com <yunpei.wang@miaocode.com>
2021-07-26 10:31:38 +0800

    颜色关联

--- .umirc.prod.js
+++ .umirc.prod.js
@@ -4,6 +4,6 @@ export default defineConfig({
     define: {
         'process.env.API_URL': 'https://we-idesign.com',
         'process.env.DOWNLOAD_URL': 'https://crm.we-idesign.com',
-        'process.env.VERSION': '1.1.2',
+        'process.env.VERSION': '1.1.3',
     },
 });
