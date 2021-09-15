ac4a0fd2d0967d886b534602d1f1c4219b7b016e
PaigeWw <767427364@qq.com>
2021-09-15 23:37:31 +0800

    fix capsule

--- .umirc.js
+++ .umirc.js
@@ -12,7 +12,7 @@ export default defineConfig({
     define: {
         'process.env.API_URL': 'http://8.209.64.159:3001',
         // 'process.env.API_URL': 'https://we-idesign.com',
-        'process.env.VERSION': '1.1.11',
+        'process.env.VERSION': '1.1.13',
         // 'process.env.API_URL': 'http://localhost:3001',
         // 'process.env.DOWNLOAD_URL': 'http://localhost:3001',
         'process.env.DOWNLOAD_URL': 'http://8.209.64.159:3001',
