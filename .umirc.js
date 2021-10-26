4d7c5fcbff24697ddbb6d28ff99e6308974932f4
PaigeWw <767427364@qq.com>
2021-10-26 22:05:59 +0800

    fix style

--- .umirc.js
+++ .umirc.js
@@ -12,7 +12,7 @@ export default defineConfig({
     define: {
         // 'process.env.API_URL': 'http://8.209.64.159:3001',
         // 'process.env.API_URL': 'https://we-idesign.com',
-        'process.env.VERSION': '1.1.16',
+        'process.env.VERSION': '1.1.20',
         'process.env.API_URL': 'http://localhost:3001',
         // 'process.env.DOWNLOAD_URL': 'http://localhost:3001',
         'process.env.DOWNLOAD_URL': 'http://8.209.64.159:3001',
