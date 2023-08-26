d4c59c0d132bdb9bc64afe6ede4065a1c6c94ec5
PaigeWw <767427364@qq.com>
2023-08-26 15:25:46 +0800

    fix: optimization

--- .umirc.prod.js
+++ .umirc.prod.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'umi';
 export default defineConfig({
     define: {
         // 'process.env.API_URL': 'http://8.209.64.159:3001',
-        'process.env.API_URL': 'https://we-idesign.com',
+        // 'process.env.API_URL': 'https://we-idesign.com',
         'process.env.DOWNLOAD_URL': 'https://crm.we-idesign.com',
-        'process.env.VERSION': '1.3.12',
+        'process.env.VERSION': '1.3.13',
     },
 });
